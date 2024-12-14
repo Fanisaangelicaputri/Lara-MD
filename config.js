aa9d351895b6cb99deef5a3ac08f947131e57792
Fanisaangelicaputri <fanisaangelicapu@gmail.com>
2024-12-14 19:11:19 +0700

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", //paste your session_id
+SESSION_ID: process.env.SESSION_ID || " 3rQDmSpD#7D4Oq0-1rQnuvUWw-AeIVIRS5OI4UP6kUwif54XBn-c", //paste your session_id
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", 
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
